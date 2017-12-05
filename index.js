5237c40747ac0002e6ca55de071c8a2a52ae80db
Giuseppe Vit <giuseppevit@gmail.com>
2017-12-05 10:18:08 +0100

    add index and hello service

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const Fastify = require('fastify')
+const minimist = require('minimist')
+const service = require('./lib/service')
+
+function start (opts) {
+  opts = opts || {}
+
+  const app = Fastify(opts)
+  app.register(service, opts)
+  app.listen(opts.port, (err) => {
+    if (err) {
+      throw err
+    }
+    
+    console.log( `Server listening on port ${app.server.address().port}`)
+  })
+}
+
+module.exports = service
+
+if (require.main === module) {
+  start(minimist(process.argv.slice(2), {
+    integer: 'port',
+    alias: {
+      'port': 'p',
+    },
+    default: {
+      port: 3000
+    }
+  }))
+}
